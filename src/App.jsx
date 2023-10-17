0d350ae13d2ef9d17fb9cfa55b161e3cd8be6c2b
Prakhar Buliya <prakharbuliya02@gmail.com>
2023-10-18 02:30:28 +0530

    Added Redux environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <div>
-      <h1>A Blog with Appwrite</h1>
+  const [loading, setLoading] = useState(true);
+  const dispach = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispach(login({ userData }));
+        } else {
+          dispach(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* Todo : <Outlet /> */}</main>
+        <Footer />
+      </div>
     </div>
-  );
+  ) : null;
 }
 
 export default App;
